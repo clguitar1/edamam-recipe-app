d1497581b407febd6bd9d2b2bae4eaa66c0d24b1
Charles Loehle <clguitar1@gmail.com>
2020-03-03 13:12:59 -0500

    set up .env file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Recipe from './Recipe';
+const api_key = process.env.REACT_APP_API_KEY;
+const api_id = process.env.REACT_APP_API_ID;
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${api_id}&app_key=${api_key}`
+    );
+
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
       </header>
+      <div className='App-content'>
+        <h1>Edamam Recipe App</h1>
+        <form onSubmit={getSearch} className='search-form'>
+          <input
+            type='text'
+            className='search-bar'
+            value={search}
+            onChange={updateSearch}
+          />
+          <button className='search-button' type='submit'>
+            Search
+          </button>
+        </form>
+
+        <button
+          onClick={() => {
+            console.log(process.env.REACT_APP_API_KEY);
+          }}
+        >
+          Secret Key
+        </button>
+        <div className='recipes'>
+          {recipes.map(recipe => (
+            <Recipe
+              key={recipe.recipe.label}
+              title={recipe.recipe.label}
+              calories={recipe.recipe.calories}
+              image={recipe.recipe.image}
+              ingredients={recipe.recipe.ingredients}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
